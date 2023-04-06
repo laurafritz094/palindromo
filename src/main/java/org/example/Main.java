3ca060f590a9db85465158c6ee891dce259e22a8
laura <laura.fritz094@gmail.com>
2023-04-06 10:49:08 -0400

    primer commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String palindromo="";
+        System.out.println("ingrese su palabra para analizar: " + palindromo);
+        boolean isPal= isPalindromo(palindromo);
+
+    }
+
+    private static boolean isPalindromo(String palindromo) {
+        boolean isPal = false;
+        String result = "";
+        result = palindromo;
+
+        return isPal;
+    }
+}
