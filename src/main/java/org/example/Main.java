46035df0712c7462c8f8dd85bbdc9e0a60ea03e9
laura <laura.fritz094@gmail.com>
2023-04-09 13:36:25 -0400

    ejecucion equivalente a la funcion

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,42 @@
 package org.example;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
 
-        String palindromo="";
-        System.out.println("ingrese su palabra para analizar: " + palindromo);
-        boolean isPal= isPalindromo(palindromo);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ingrese su palabra para analizar: ");
+        String palindrome=sc.next();
+        boolean isPal= isPalindromo(palindrome);
 
+        System.out.println("resultado boolean {}"+isPal);
+        printResult(isPal);
+
+    }
+
+    private static void printResult(boolean isPal) {
+        if (isPal){
+            System.out.println("la palabra es un palindromo");
+        } else {
+            System.out.println("la palabra no es palindromo");
+        }
     }
 
     private static boolean isPalindromo(String palindromo) {
         boolean isPal = false;
-        String result = "";
-        result = palindromo;
+        new ArrayList<>(Arrays.asList("RED", "BLUE", "BLACK"));
+        List<String> list = new ArrayList<>(List.of(palindromo.split("")));
+        Collections.reverse(list);
+        StringBuilder result = new StringBuilder();
+
+        for(String cadena :list) {
+            System.out.println(cadena);
+            result.append(cadena);
+        }
+        if(palindromo.equals(result.toString())){
+            isPal=true;
+        }
 
         return isPal;
     }
